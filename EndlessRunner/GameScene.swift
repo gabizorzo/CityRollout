e318b80b97cec6817b4343c57a5c46b0c3c8cf2a
gabizorzo <gabriela.zorzo@edu.pucrs.br>
2024-07-17 18:38:47 -0300

    fix: ajuste no movimento para ficar constante

--- EndlessRunner/GameScene.swift
+++ EndlessRunner/GameScene.swift
@@ -13,11 +13,15 @@ class GameScene: SKScene {
     private var player = SKShapeNode(circleOfRadius: 15)
     
     // MARK: - Sizes
-    private var screenHeight = UIScreen.main.bounds.height
-    private var screenWidth = UIScreen.main.bounds.width
+    private let screenHeight = UIScreen.main.bounds.height
+    private let screenWidth = UIScreen.main.bounds.width
     
     // MARK: - Collision Categories
     let playerCategory: UInt32 = 1 << 5
+    
+    // MARK: - Variables
+    var isTouching: Bool = false
+    var touchLocation: CGFloat = 0.0
     
     override func didMove(to view: SKView) {
         self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
@@ -42,19 +46,26 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else {
-            return
-        }
+        guard let touch = touches.first else { return }
         
-        let location = touch.location(in: self)
-        if location.x > 0 {
-            self.player.run(SKAction.move(by: CGVector(dx: 15, dy: 0), duration: 0.05))
-        } else {
-            self.player.run(SKAction.move(by: CGVector(dx: -15, dy: 0), duration: 0.05))
-        }
+        touchLocation = touch.location(in: self).x
+        isTouching = true
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        
+        isTouching = false
     }
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        // MARK: Player movement
+        if isTouching {
+            if touchLocation > 0 {
+                self.player.run(SKAction.move(by: CGVector(dx: 2, dy: 0), duration: 0.05))
+            } else {
+                self.player.run(SKAction.move(by: CGVector(dx: -2, dy: 0), duration: 0.05))
+            }
+        }
     }
 }
