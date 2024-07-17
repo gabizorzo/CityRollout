87a6a1ff7d21e00ba740c14a74f74d7571632ddc
gabizorzo <gabriela.zorzo@edu.pucrs.br>
2024-07-17 19:19:50 -0300

    feat: gerando obstáculos

--- EndlessRunner/GameScene.swift
+++ EndlessRunner/GameScene.swift
@@ -18,10 +18,12 @@ class GameScene: SKScene {
     
     // MARK: - Collision Categories
     let playerCategory: UInt32 = 1 << 5
+    let obstaclesCategory: UInt32 = 1 << 4
     
     // MARK: - Variables
-    var isTouching: Bool = false
-    var touchLocation: CGFloat = 0.0
+    private var isTouching: Bool = false
+    private var touchLocation: CGFloat = 0.0
+    private var lastCurrentTime: Double = -1
     
     override func didMove(to view: SKView) {
         self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
@@ -29,14 +31,18 @@ class GameScene: SKScene {
         createPlayer()
     }
     
+    // MARK: - Create nodes
     func createSceneBoundingBox() {
         self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
     }
     
     func createPlayer() {
+        self.player.name = "Player"
         self.player.fillColor = .green
         self.player.strokeColor = .clear
         self.player.position = CGPoint(x: 0, y: -(screenHeight/2.5))
+        self.player.zPosition = 1
+        
         let physics = SKPhysicsBody(circleOfRadius: (self.player.path?.boundingBox.width)!/2)
         physics.categoryBitMask = playerCategory
         physics.affectedByGravity = false
@@ -45,6 +51,61 @@ class GameScene: SKScene {
         self.addChild(player)
     }
     
+    func createObstacles() {
+        let obstacleSize = 15
+        let obstacle = SKShapeNode(rectOf: CGSize(width: obstacleSize, height: obstacleSize))
+        obstacle.name = "Obstacle"
+        obstacle.strokeColor = .clear
+        obstacle.fillColor = .red
+        
+        let x = getPosition()
+        let obstaclePosition = CGPoint(x: x, y: screenHeight/2)
+        obstacle.position = obstaclePosition
+        obstacle.zPosition = 1
+        
+        let physics = SKPhysicsBody(rectangleOf: CGSize(width: obstacleSize, height: obstacleSize))
+        physics.categoryBitMask = obstaclesCategory
+        physics.affectedByGravity = false
+        obstacle.physicsBody = physics
+        
+        addChild(obstacle)
+    }
+}
+
+// MARK: - Moves
+extension GameScene {
+    func movePlayer() {
+        if isTouching {
+            if touchLocation > 0 {
+                self.player.run(SKAction.move(by: CGVector(dx: 2, dy: 0), duration: 0.05))
+            } else {
+                self.player.run(SKAction.move(by: CGVector(dx: -2, dy: 0), duration: 0.05))
+            }
+        }
+    }
+    
+    func moveObstacles() {
+        self.enumerateChildNodes(withName: "Obstacle") { node, error in
+            node.position.y -= 2 // aqui que define a velocidade na qual serão movidos os obstáculos
+            if node.position.y <= -self.screenHeight/2 + 15 { // 15 é o tamanho do obstáculo, ajustar depois
+                node.removeFromParent()
+            }
+        }
+    }
+}
+
+// MARK: - Help funcs
+extension GameScene {
+    func getPosition() -> CGFloat {
+        let min = -screenWidth/2 + 15 // 15 é o tamanho do obstáculo, ajustar depois
+        let max = screenWidth/2 - 15 // 15 é o tamanho do obstáculo, ajustar depois
+        let value = Int.random(in: Int(min)..<Int(max))
+        return CGFloat(value)
+    }
+}
+
+// MARK: - Touches
+extension GameScene {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
         
@@ -53,19 +114,31 @@ class GameScene: SKScene {
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else { return }
+        guard touches.first != nil else { return }
         
         isTouching = false
     }
-    
+}
+
+// MARK: - Update
+extension GameScene {
     override func update(_ currentTime: TimeInterval) {
         // MARK: Player movement
-        if isTouching {
-            if touchLocation > 0 {
-                self.player.run(SKAction.move(by: CGVector(dx: 2, dy: 0), duration: 0.05))
-            } else {
-                self.player.run(SKAction.move(by: CGVector(dx: -2, dy: 0), duration: 0.05))
-            }
+        movePlayer()
+        
+        // MARK: Generate obstacles
+        if lastCurrentTime == -1 {
+            lastCurrentTime = currentTime
         }
+        
+        let deltaTime = currentTime - lastCurrentTime
+        
+        if deltaTime > 2.5 { // aqui que define a velocidade na qual serão gerados novos obstáculos
+            createObstacles()
+            lastCurrentTime = currentTime
+        }
+        
+        // MARK: Obstacles movement
+        moveObstacles()
     }
 }
