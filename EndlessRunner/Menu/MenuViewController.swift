87cb7a1ca1c2408ffb2f728082fbdcad89b55842
jdallagnol <joao.dallagnol1@gmail.com>
2024-09-13 15:39:20 -0300

    feat: menu buttons and bg asset

--- EndlessRunner/Menu/MenuViewController.swift
+++ EndlessRunner/Menu/MenuViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class MenuViewController: UIViewController {
     @IBOutlet weak var backgroundImage: UIImageView!
     @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var howToPlayButton: UIButton!
     @IBOutlet weak var settingsButton: UIButton!
         
     //MARK: - Settings button tap feedback
@@ -22,13 +23,21 @@ class MenuViewController: UIViewController {
         backgroundImage.frame.size = UIScreen.main.bounds.size
         
         startButton.setTitle(String(localized: "menuView.play"), for: .normal)
-        startButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
+//        startButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        startButton.setTitleColor(.white, for: .normal)
         startButton.titleLabel?.numberOfLines = 0
         startButton.titleLabel?.lineBreakMode = .byCharWrapping
         startButton.autoresizingMask = [.flexibleHeight, .flexibleWidth]
 
+        howToPlayButton.setTitle("How to play", for: .normal)
+        howToPlayButton.setTitleColor(.white, for: .normal)
+        howToPlayButton.titleLabel?.numberOfLines = 0
+        howToPlayButton.titleLabel?.lineBreakMode = .byCharWrapping
+        howToPlayButton.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        
         settingsButton.setTitle(String(localized: "menuView.settings"), for: .normal)
-        settingsButton.setImage(UIImage(systemName: "gearshape.fill"), for: .normal)
+//        settingsButton.setImage(UIImage(systemName: "gearshape.fill"), for: .normal)
+        settingsButton.setTitleColor(.white, for: .normal)
         settingsButton.titleLabel?.numberOfLines = 0
         settingsButton.titleLabel?.lineBreakMode = .byCharWrapping
         settingsButton.autoresizingMask = [.flexibleHeight, .flexibleWidth]
